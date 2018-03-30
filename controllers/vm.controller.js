d2484be43a8168d677c1a57cab1122c09259ceb9
mouyaq <mouyaq@gmail.com>
2018-03-30 18:47:25 +0200

    feat: vm details

--- controllers/vm.controller.js
+++ controllers/vm.controller.js
@@ -21,3 +21,23 @@ module.exports.list = (req, res, next) => {
         res.status(500).json(error);
     });
 }
+
+module.exports.details = (req, res, next) => {
+    const id = req.params.id;
+    my_http_options = {
+        method: 'get',
+        url: `https://${process.env.VC}/rest/vcenter/vm/${id}`,
+        withCredentials: true,
+        headers: req.headers,
+        responseType: 'json',
+        responseEncoding: 'utf8',
+        httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+    }
+    axios(my_http_options)
+    .then( response => {
+        res.status(200).json(response.data);
+    })
+    .catch( error => {
+        res.status(500).json(error);
+    });
+}
--- routes/vm.routes.js
+++ routes/vm.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const vmController = require('../controllers/vm.controller');
 
 router.get('/', vmController.list);
+router.get('/:id', vmController.details);
 
 module.exports = router;
