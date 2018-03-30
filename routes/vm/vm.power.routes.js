b3beb80c39cbcd07019a8ff342210aee62aeb0a9
mouyaq <mouyaq@gmail.com>
2018-03-30 19:13:07 +0200

    fix: route vm power

--- routes/vm/vm.power.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const vmPowerController = require('../../controllers/vm/vm.power.controller');
-
-router.get('/', vmPowerController.details);
-
-module.exports = router;
--- routes/vm/vm.routes.js
+++ routes/vm/vm.routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const vmController = require('../../controllers/vm/vm.controller');
+const vmPowerController = require('../../controllers/vm/vm.power.controller');
 
 router.get('/', vmController.list);
+router.get('/:id/power', vmPowerController.details);
 router.get('/:id', vmController.details);
 
 module.exports = router;
