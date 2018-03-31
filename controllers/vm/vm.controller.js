13c51fe490f6f0120b912f82f24ff77315255c6b
mouyaq <mouyaq@gmail.com>
2018-03-31 02:07:52 +0200

    feat: vm create

--- controllers/vm/vm.controller.js
+++ controllers/vm/vm.controller.js
@@ -22,6 +22,25 @@ module.exports.list = (req, res, next) => {
     });
 }
 
+module.exports.create = (req, res, next) => {
+    my_http_options = {
+        method: 'post',
+        url: `https://${process.env.VC}/rest/vcenter/vm`,
+        withCredentials: true,
+        headers: req.headers,
+        responseType: 'json',
+        responseEncoding: 'utf8',
+        httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+    }
+    axios(my_http_options)
+    .then( response => {
+        res.status(200).json(response.data);
+    })
+    .catch( error => {
+        res.status(500).json(error);
+    });
+}
+
 module.exports.details = (req, res, next) => {
     const id = req.params.id;
     my_http_options = {
--- routes/vm/vm.routes.js
+++ routes/vm/vm.routes.js
@@ -5,6 +5,7 @@ const vmController = require('../../controllers/vm/vm.controller');
 const vmPowerController = require('../../controllers/vm/vm.power.controller');
 
 router.get('/', vmController.list);
+router.post('/', vmController.create);
 router.get('/:id/power', vmPowerController.details);
 router.post('/:id/power/reset', vmPowerController.reset);
 router.post('/:id/power/start', vmPowerController.start);
