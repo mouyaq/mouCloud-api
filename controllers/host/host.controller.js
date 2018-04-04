2f048150db960ebec8f8f21ca66d833a44909d72
mouyaq <mouyaq@gmail.com>
2018-04-05 01:06:09 +0200

    feat: added host filter to find hosts

--- controllers/host/host.controller.js
+++ controllers/host/host.controller.js
@@ -4,9 +4,17 @@ const axios = require('axios');
 const https = require('https');
 
 module.exports.list = (req, res, next) => {
+    let url;
+    const filters = req._parsedUrl.search;
+    if(filters != null) {
+        url = `https://${process.env.VC}/rest/vcenter/host${filters}`;
+    }
+    else {
+        url = `https://${process.env.VC}/rest/vcenter/host`;
+    }
     my_http_options = {
         method: 'get',
-        url: `https://${process.env.VC}/rest/vcenter/host`,
+        url: url,
         withCredentials: true,
         headers: req.headers,
         responseType: 'json',
