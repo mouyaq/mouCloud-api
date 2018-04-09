d01493b77cfa0182fdcc556482df4a73bfab752a
mouyaq <mouyaq@gmail.com>
2018-04-09 04:13:39 +0200

    feat: add set-cookie for logout

--- controllers/session/session.controller.js
+++ controllers/session/session.controller.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 const axios = require('axios');
 const https = require('https');
 
-var a;
-
 module.exports.create = (req, res, next) => {
     //Set-Cookie →vmware-api-session-id=ddfabc5f9235f121e2a258fa6c1d07df;Path=/rest;Secure;HttpOnly
     const username = req.body.username;
@@ -28,16 +26,15 @@ module.exports.create = (req, res, next) => {
             my_http_options.headers = {
                 'Cookie': cookieSession
             };
-            a = cookieSession;
             const user = {
                 username: my_http_options.auth.username,
-                token: response.data.value
+                token: response.data.value,
+                cookie: cookieSession
             }
             my_http_options.auth = {};
             // change from {"value":"key"} to {"vmware-api-session-id":"key"}
             //response.data["vmware-api-session-id"] = response.data.value;
             //delete response.data.value;
-            console.log(user);
             res.status(201).json(user);
         })
         .catch( error => {
@@ -59,14 +56,14 @@ module.exports.destroy = (req, res, next) => {
         responseType: 'json',
         responseEncoding: 'utf8',
         httpsAgent: new https.Agent({ rejectUnauthorized: false }),
-        headers: { 'Cookie': a }
+        headers: { 'Cookie': req.headers['set-cookie'] }
     }
     axios(my_http_options)
         .then( response => {
             res.status(204).json(response.data);
         })
         .catch( error => {
-            console.log(error);
+            //console.log(error);
             if(error.response.data.type === "com.vmware.vapi.std.errors.unauthenticated") {
                 res.status(401).json(error.response.data);
             }
