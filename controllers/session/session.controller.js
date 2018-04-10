e4434c3d648f08d8143483bc740a131043d77c2b
mouyaq <mouyaq@gmail.com>
2018-04-11 00:22:31 +0200

    fix: clean code

--- controllers/session/session.controller.js
+++ controllers/session/session.controller.js
@@ -4,7 +4,6 @@ const axios = require('axios');
 const https = require('https');
 
 module.exports.create = (req, res, next) => {
-    //Set-Cookie →vmware-api-session-id=ddfabc5f9235f121e2a258fa6c1d07df;Path=/rest;Secure;HttpOnly
     const username = req.body.username;
     const password = req.body.password;
     my_http_options = {
@@ -21,9 +20,7 @@ module.exports.create = (req, res, next) => {
     }
     axios(my_http_options)
         .then( response => {
-            // cookie value
             const cookieSession = response.headers['set-cookie'];
-            console.log(cookieSession[0]);
             my_http_options.headers = {
                 'Cookie': cookieSession[0]
             };
@@ -33,9 +30,6 @@ module.exports.create = (req, res, next) => {
                 cookie: cookieSession[0]
             }
             my_http_options.auth = {};
-            // change from {"value":"key"} to {"vmware-api-session-id":"key"}
-            //response.data["vmware-api-session-id"] = response.data.value;
-            //delete response.data.value;
             res.status(201).json(user);
         })
         .catch( error => {
@@ -58,7 +52,6 @@ module.exports.destroy = (req, res, next) => {
         responseType: 'json',
         responseEncoding: 'utf8',
         httpsAgent: new https.Agent({ rejectUnauthorized: false }),
-        //headers: { 'Cookie': req.headers['set-cookie'] }
         headers: req.headers
     }
     axios(my_http_options)
@@ -66,7 +59,6 @@ module.exports.destroy = (req, res, next) => {
             res.status(204).json(response.data);
         })
         .catch( error => {
-            //console.log(error);
             if(error.response.data.type === "com.vmware.vapi.std.errors.unauthenticated") {
                 res.status(401).json(error.response.data);
             }
@@ -75,15 +67,3 @@ module.exports.destroy = (req, res, next) => {
             }
         });
 }
-
-
-// response: 
-//     { 
-//         status: 401,
-//         statusText: 'Unauthorized',
-//         data:
-//             { 
-//                 type: 'com.vmware.vapi.std.errors.unauthenticated',
-//                 value: [Object] 
-//             } 
-//     }
