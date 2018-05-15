32f8ec5c461228a220ec2ac1b52d9b9586a70187
mouyaq <mouyaq@gmail.com>
2018-05-15 23:48:11 +0200

    feat: items route list

--- controllers/content/library.controller.js
+++ controllers/content/library.controller.js
@@ -35,3 +35,53 @@ module.exports.list = (req, res, next) => {
     });
 
 }
+
+module.exports.details = (req, res, next) => {
+    const id = req.params.id;
+    my_http_options = {
+        method: 'get',
+        url: `https://${process.env.VC}/rest/com/vmware/content/library/id:${id}`,
+        withCredentials: true,
+        headers: req.headers,
+        responseType: 'json',
+        responseEncoding: 'utf8',
+        httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+    }
+    axios(my_http_options)
+    .then( response => {
+        res.status(200).json(response.data.value);
+    })
+    .catch( error => {
+        if(error.response.data.type === "com.vmware.vapi.std.errors.unauthenticated") {
+            res.status(401).json(error.response.data);
+        }
+        else {
+            res.status(500).json(error.response.data);
+        }
+    });
+}
+
+module.exports.items = (req, res, next) => {
+    const id = req.params.id;
+    my_http_options = {
+        method: 'get',
+        url: `https://${process.env.VC}/rest/com/vmware/content/library/item?library_id=${id}`,
+        withCredentials: true,
+        headers: req.headers,
+        responseType: 'json',
+        responseEncoding: 'utf8',
+        httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+    }
+    axios(my_http_options)
+    .then( response => {
+        res.status(200).json(response.data.value);
+    })
+    .catch( error => {
+        if(error.response.data.type === "com.vmware.vapi.std.errors.unauthenticated") {
+            res.status(401).json(error.response.data);
+        }
+        else {
+            res.status(500).json(error.response.data);
+        }
+    });
+}
--- routes/content/library.routes.js
+++ routes/content/library.routes.js
@@ -4,5 +4,18 @@ const router = express.Router();
 const contentLibraryController = require('../../controllers/content/library.controller');
 
 router.get('/', contentLibraryController.list);
+router.get('/:id', contentLibraryController.details);
+// returns all items from content library=id
+router.get('/items/:id', contentLibraryController.items);
+
+// router.post('/', vmController.create);
+// router.get('/:id/power', vmPowerController.details);
+// router.post('/:id/power/reset', vmPowerController.reset);
+// router.post('/:id/power/start', vmPowerController.start);
+// router.post('/:id/power/stop', vmPowerController.stop);
+// router.get('/:id', vmController.details);
+// router.delete('/:id', vmController.delete);
+// router.get('/:id/console', vmController.console);
+// router.get('/:id/consolePython', vmController.consolePython);
 
 module.exports = router;
