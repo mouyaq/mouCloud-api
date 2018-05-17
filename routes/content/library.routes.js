78d296f7af9c68ef3cb888ef27a9df526e7c4bf4
mouyaq <mouyaq@gmail.com>
2018-05-17 04:05:38 +0200

    fix: library routes

--- routes/content/library.routes.js
+++ routes/content/library.routes.js
@@ -10,14 +10,4 @@ router.get('/:id/items/', contentLibraryController.items);
 router.get('/item/:id', contentLibraryController.itemDetails);
 router.post('/deployItem/:id', contentLibraryController.deployItem);
 
-// router.post('/', vmController.create);
-// router.get('/:id/power', vmPowerController.details);
-// router.post('/:id/power/reset', vmPowerController.reset);
-// router.post('/:id/power/start', vmPowerController.start);
-// router.post('/:id/power/stop', vmPowerController.stop);
-// router.get('/:id', vmController.details);
-// router.delete('/:id', vmController.delete);
-// router.get('/:id/console', vmController.console);
-// router.get('/:id/consolePython', vmController.consolePython);
-
 module.exports = router;
